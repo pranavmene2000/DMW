59679e332576d26807fffc28d48700be321a8906
Prathamesh Kurunkar <kurunkarprathamesh7@gmail.com>
2021-10-19 19:11:37 +0530

    fix: changed few styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react'
 import { Container, Typography } from '@material-ui/core'
 import { useState } from 'react';
 import Searchbar from './components/Searchbar/Searchbar';
@@ -7,42 +8,47 @@ import Button from './components/Button/Button';
 import SearchIcon from '@material-ui/icons/Search';
 import TweetsContainer from './components/TweetsContainer/TweetsContainer';
 import Loader from './components/Loader/Loader';
+import ErrorSnackbar from './components/ErrorSnackbar/ErrorSnackbar';
 
 function App() {
   const [text, setText] = useState("");
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(100);
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleClick = () => {
-    setLoading(true);
-    fetch('./data.json', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    })
-      .then(function (response) {
-        console.log(response)
-        return response.json();
-      })
-      .then(function (myJson) {
-        console.log(myJson)
-        const D = {
-          negative: myJson.negative,
-          positive: myJson.positive,
-          ntweets: myJson.ntweets.filter(c => c.includes(text)),
-          ptweets: myJson.ptweets.filter(c => c.includes(text)),
+    if(text.trim().length <= 2){
+      setError("Please Enter a query with more than 2 characters!")
+    }else{
+      setLoading(true);
+      fetch(`http://localhost:5000/search/?query=${text}&count=${count}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
         }
-        setData(D);
-        setTimeout(() => setLoading(false), 1200)
-      });
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (myJson) {
+          const D = {
+            negative: myJson.negative,
+            positive: myJson.positive,
+            ntweets: myJson.ntweets,
+            ptweets: myJson.ptweets
+          }
+          setData(D);
+          setLoading(false);
+        })
+    }
+    
   }
 
   return (
     <Container>
-      <Typography align="center" color="textSecondary" variant="p" component="h1">
-        Opinion miner for tweets 🍉
+      <Typography align="center" color="textSecondary" variant="h4">
+        Twitter Opinion Miner
       </Typography>
       <Searchbar text={text} setText={setText} setData={setData} />
       <Select count={count} setCount={setCount} />
@@ -60,6 +66,7 @@ function App() {
       {loading ? (
         <Loader />
       ) : <TweetsContainer data={data} />}
+      <ErrorSnackbar error={error} setError={setError}/>
     </Container>
   );
 }
